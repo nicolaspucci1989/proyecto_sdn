ff1ab3b2a157fccf01ef2adcd023f9eac1aee425
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-08-30 13:23:39 -0300

    agregando listas de hosts y switches a topologia

--- topo2.py
+++ topo2.py
@@ -20,24 +20,24 @@ class RYU( Controller ):
 class SingleSwitchTopo(Topo):
 
     def build(self):
-        hosts = []
-        switch = []
+        self.hsts = []
+        self.swchs = []
 
         for h in range(5):
-            hosts.append(self.addHost('h%s' % (h + 1),
+            self.hsts.append(self.addHost('h%s' % (h + 1),
                 mac='00:00:00:00:00:0%s' % ( h + 1 )))
 
         for s in range(3):
-            switch.append(self.addSwitch('s%s' % (s + 1)))
+            self.swchs.append(self.addSwitch('s%s' % (s + 1)))
 
-        self.addLink(hosts[0], switch[0])
-        self.addLink(hosts[1], switch[0])
-        self.addLink(hosts[2], switch[1])
-        self.addLink(hosts[4], switch[2])
-        self.addLink(hosts[3], switch[1])
+        self.addLink(self.hsts[0], self.swchs[0])
+        self.addLink(self.hsts[1], self.swchs[0])
+        self.addLink(self.hsts[2], self.swchs[1])
+        self.addLink(self.hsts[4], self.swchs[2])
+        self.addLink(self.hsts[3], self.swchs[1])
 
-        self.addLink(switch[0], switch[1])
-        self.addLink(switch[1], switch[2])
+        self.addLink(self.swchs[0], self.swchs[1])
+        self.addLink(self.swchs[1], self.swchs[2])
 
 
 if __name__ == '__main__':
