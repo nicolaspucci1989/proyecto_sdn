72227f55ccd13c11730e9601a32327691261ccf4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-08-31 18:52:23 -0300

    cambiando orden de switch

--- topo2.py
+++ topo2.py
@@ -33,24 +33,41 @@ class SingleSwitchTopo(Topo):
         self.addLink(self.hsts[0], self.swchs[0])
         self.addLink(self.hsts[1], self.swchs[0])
         self.addLink(self.hsts[2], self.swchs[1])
-        self.addLink(self.hsts[4], self.swchs[2])
         self.addLink(self.hsts[3], self.swchs[1])
+        self.addLink(self.hsts[4], self.swchs[2])
 
         self.addLink(self.swchs[0], self.swchs[1])
         self.addLink(self.swchs[1], self.swchs[2])
 
 
-if __name__ == '__main__':
+def testSencillo():
     topo = SingleSwitchTopo()
-    # Asignar topologia y puerto para acceder con dpctl,
-    # si no usar ovs-ofctl
-    # ej, s1 dpctl dump-talbes tcp:127.0.0.1:6634
-    # Puerto por defecto 6633
     setLogLevel( 'info' )
     net = Mininet(topo, listenPort=6634, controller=RYU)
     net.start()
+    h1 = net.hosts[ 0 ]
+    h2 = net.hosts[ 1 ]
+    s1 = net.get( 's1' )
+    #print h1.cmd( 'ping -c3', h2.IP() )
+    s1.cmd('dpctl add-flow tcp:127.0.0.1:6634 in_port=1,        actions=output:2' )
+    s1.cmd('dpctl add-flow tcp:127.0.0.1:6634 in_port=2,        actions=output:1' )
     CLI(net)
+    #print s1.cmd( 'dpctl dump-flows tcp:127.0.0.1:6634' )
+    #print h1.cmd( 'ping -c3', h2.IP() )
     net.stop()
+
+if __name__ == '__main__':
+    testSencillo()
+    #topo = SingleSwitchTopo()
+    ## Asignar topologia y puerto para acceder con dpctl,
+    ## si no usar ovs-ofctl
+    ## ej, s1 dpctl dump-talbes tcp:127.0.0.1:6634
+    ## Puerto por defecto 6633
+    #setLogLevel( 'info' )
+    #net = Mininet(topo, listenPort=6634, controller=RYU)
+    #net.start()
+    #CLI(net)
+    #net.stop()
 
 else: # Si no es main, es argumento de mn
     topos = {'mytopo': ( lambda: SingleSwitchTopo() )}
