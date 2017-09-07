2c9a5514324d73391785f7f9217ddf2bdff1d30c
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-07 20:59:58 -0300

    agregando comentarios

--- switch_simple.py
+++ switch_simple.py
@@ -2,18 +2,26 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_0, ofproto_v1_3
 
 class SimpleHub(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
-    #OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    #En version 1.0, si hay un table miss la accion por defecto es enviarla al controlador
+    #En version 1.3, si hay un table miss la accion por defecto es dropearlo
+
+
+    #OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+
     def __init__(self, *args, **kwargs):
         super(SimpleHub, self).__init__(*args, **kwargs)
+
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         message = ev.msg
         print("message: ", message)
+
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
