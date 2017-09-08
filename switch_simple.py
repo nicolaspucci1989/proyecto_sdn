b640475eb2d7fddf2bf691792d3b18d6f8cfb1c4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-08 16:32:00 -0300

    switch smple con add flow

--- switch_simple.py
+++ switch_simple.py
@@ -1,56 +1,39 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
- #from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+import array
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+#from ryu.lib.packet import ipv4, tcp
 
-class SimpleHub(app_manager.RyuApp):
-    #En version 1.0, si hay un table miss la accion por defecto es enviarla al controlador
-    #En version 1.3, si hay un table miss la accion por defecto es dropearlo
 
-
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
-    #OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleHub, self).__init__(*args, **kwargs)
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
 
-    #Decorador del event handler, toma dos argumentos. El primero indica que evento invocara a la
-    #funcion. El segundo indica el estado del switch
-    #En este caso estamos invocando la funcion cuando hay un evento "Switch Features"; CONFIG_DISPATCHER
-    #significa que el switch esta en la fase de negociacion de version y envio del mensaje de
-    #"features-request"
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        message = ev.msg
-        print("message: ", message)
-
-
-    #MAIN_DISPATCHER indica que la funcion debe ser llamada despues que la negociacion de features
-    #se haya completado
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        msg = ev.msg
-        print("ev message: ", ev.msg)
-
-        datapath = msg.datapath
+        datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-        out = parser.OFPPacketOut(
-                datapath=datapath,
-                buffer_id=msg.buffer_id,
-                in_port=msg.in_port,
-                actions=actions)
-        datapath.send_msg(out)
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                            actions)]
+                                             actions)]
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
@@ -58,5 +41,65 @@ class SimpleHub(app_manager.RyuApp):
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
-
         datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+
+        # Decodificar e imprimir paquete
+        print("msg.data: {}".format(array.array('B', ev.msg.data)))
+        pkt = packet.Packet(ev.msg.data)
+        for p in pkt.protocols:
+            print(p.protocol_name, p)
+            if p.protocol_name == 'ipv4':
+                print('IP: src {} dst {}'.format(p.src, p.dst))
+
+        # Below is the original code from simple_switch_13.py
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
