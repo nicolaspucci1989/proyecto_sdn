71f922c8e1028defe660aa52b86d18d8a373c890
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-08 13:24:00 -0300

    agregando script mininet y add_flow a swithc

--- switch_simple.py
+++ switch_simple.py
@@ -45,3 +45,17 @@ class SimpleHub(app_manager.RyuApp):
                 in_port=msg.in_port,
                 actions=actions)
         datapath.send_msg(out)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+            actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                    priority=priority, match=match,
+                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                    match=match, instructions=inst)
+            datapath.send_msg(mod)
