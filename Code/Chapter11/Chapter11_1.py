b5bb4eee51a2a38d91a0a7f302e41c7cc7860f79
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-08 15:30:43 -0300

    cambios menores

--- Code/Chapter11/Chapter11_1.py
+++ Code/Chapter11/Chapter11_1.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
@@ -15,9 +15,12 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
 
-        print("EventOFPSwitchFeatures: datapath_id {}, n_buffers {} capabilities {}".format(ev.msg.datapath_id,
-                                                                                            ev.msg.n_buffers,
-                                                                                            ev.msg.capabilities))
+        print("EventOFPSwitchFeatures: datapath_id {}, n_buffers {} capabilities {}".format(ev.msg.
+                                                                                            datapath_id,
+                                                                                            ev.msg.
+                                                                                            n_buffers,
+                                                                                            ev.msg.
+                                                                                            capabilities))
 
 
 
