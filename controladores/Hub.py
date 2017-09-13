db873f9c363a8e334befaa8e57208ca18051979e
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-13 11:55:15 -0300

    cambios menores

--- controladores/Hub.py
+++ controladores/Hub.py
@@ -5,28 +5,29 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 
 
-class Hub(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+class Hub( app_manager.RyuApp ):
+    OFP_VERSIONS = [ ofproto_v1_0.OFP_VERSION ]
 
-    def __init__(self, *args, **kwargs):
-        super(Hub, self).__init__(*args, **kwargs)
+    def __init__( self, *args, **kwargs ):
+        super( Hub, self ).__init__( *args, **kwargs )
 
     #ofp_event.EventOFPPacketIn
     #   que evento escuchar
     #MAIN_DISPATCHER
     #   despues de la negociacion inicial
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handleR(self, evento):
+    @set_ev_cls( ofp_event.EventOFPPacketIn, MAIN_DISPATCHER )
+    def packet_in_handleR( self, evento ):
         mensaje = evento.msg #Estructura del packet_in
         dp = mensaje.datapath #Estructura que representa el switch
         ofp = dp.ofproto #Protocolo negociado entre switch y contro
         ofp_parser = dp.ofproto_parser
         puerto_entrada = mensaje.in_port
 
-        acciones = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+        acciones = [ ofp_parser.OFPActionOutput( ofp.OFPP_FLOOD ) ]
 
         out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=mensaje.buffer_id,
-            in_port=puerto_entrada,
-            actions=acciones)
-        dp.send_msg(out)
+                    datapath=dp, buffer_id=mensaje.buffer_id,
+                    in_port=puerto_entrada,
+                    actions=acciones
+                    )
+        dp.send_msg( out )
