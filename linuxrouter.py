d369230febe3997b06e8fc077aefe56c81fbd8e5
Nicolas Pucci <nicolaspucci1989@gmail.com>
2017-09-14 01:14:02 -0300

    agregando hosts

--- linuxrouter.py
+++ linuxrouter.py
@@ -35,28 +35,28 @@ from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
 
-class LinuxRouter( Node ):
-    "A Node with IP forwarding enabled."
+class RouterSimple( Node ):
+    "Nodo con IP forwarding habilitado."
 
     def config( self, **params ):
-        super( LinuxRouter, self).config( **params )
-        # Enable forwarding on the router
+        super( RouterSimple, self).config( **params )
+        # Habilitar IP forwarding
         self.cmd( 'sysctl net.ipv4.ip_forward=1' )
 
     def terminate( self ):
         self.cmd( 'sysctl net.ipv4.ip_forward=0' )
-        super( LinuxRouter, self ).terminate()
+        super( RouterSimple, self ).terminate()
 
 
 class NetworkTopo( Topo ):
-    "A LinuxRouter connecting three IP subnets"
+    "El router conecta tres subredes"
 
     def build( self, **_opts ):
 
         defaultIP = '192.168.1.1/24'  # IP address for r0-eth1
-        router = self.addNode( 'r0', cls=LinuxRouter, ip=defaultIP )
+        router = self.addNode( 'r0', cls=RouterSimple, ip=defaultIP )
 
-        s1, s2, s3 = [ self.addSwitch( s ) for s in ( 's1', 's2', 's3' ) ]
+        s1, s2, s3, = [ self.addSwitch( s ) for s in ( 's1', 's2', 's3' ) ]
 
         self.addLink( s1, router, intfName2='r0-eth1',
                       params2={ 'ip' : defaultIP } )  # for clarity
@@ -67,12 +67,16 @@ class NetworkTopo( Topo ):
 
         h1 = self.addHost( 'h1', ip='192.168.1.100/24',
                            defaultRoute='via 192.168.1.1' )
-        h2 = self.addHost( 'h2', ip='172.16.0.100/12',
+        h2 = self.addHost( 'h2', ip='192.168.1.200/24',
+                           defaultRoute='via 192.168.1.1' )
+        h3 = self.addHost( 'h3', ip='172.16.0.100/12',
                            defaultRoute='via 172.16.0.1' )
-        h3 = self.addHost( 'h3', ip='10.0.0.100/8',
+        h4 = self.addHost( 'h4', ip='172.16.0.200/12',
+                           defaultRoute='via 172.16.0.1' )
+        h5 = self.addHost( 'h5', ip='10.0.0.100/8',
                            defaultRoute='via 10.0.0.1' )
 
-        for h, s in [ (h1, s1), (h2, s2), (h3, s3) ]:
+        for h, s in [ (h1, s1), (h2, s1), (h3, s2), (h4, s2), (h5, s3) ]:
             self.addLink( h, s )
 
 
